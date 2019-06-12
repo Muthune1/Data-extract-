393a7eb6596a6d64dfbc6f6899b72a39a3b4f655
Muthune1 <40231559+Muthune1@users.noreply.github.com>
2019-06-12 13:38:32 +0530

    Delete Logdata_old_unity.py

--- Logdata_old_unity.py
+++
@@ -1,9 +0,0 @@
-import os
-#import getpass
-#from subprocess import check_output
-
-
-os.system("python generatereport_unity_Old.py")          #Adjust date,add dimensions,metricss in this report as needed
-os.system("C:\snowsql -f logdata_old_unity.sql")
-
-
