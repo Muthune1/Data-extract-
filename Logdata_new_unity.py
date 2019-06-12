76f8e54bbf8b8e0d4b071e472805ed0c03b77696
Muthune1 <40231559+Muthune1@users.noreply.github.com>
2019-06-12 13:38:19 +0530

    Delete Logdata_new_unity.py

--- Logdata_new_unity.py
+++
@@ -1,9 +0,0 @@
-import os
-#import getpass
-#from subprocess import check_output
-
-
-os.system("python generatereport_unity_4d.py")          #Adjust date,add dimensions,metricss in this report as needed
-os.system("C:\snowsql -f logdata_new_unity.sql")
-
-
